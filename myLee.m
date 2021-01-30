a2e87bd601a819d1fd7f4d73aeb43fcd9ff7ac38
KRISHNA0905 <76141140+KRISHNA0905@users.noreply.github.com>
2021-01-29 20:30:47 -0500

    Add files via upload

---
+++ myLee.m
@@ -0,0 +1,11 @@
+function OIm=myLee(I)
+I = double(I);
+OIm = I;
+window_size = 3;
+means = imfilter(I, fspecial('average', window_size), 'replicate');
+sigmas = sqrt((I-means).^2/window_size^2);
+sigmas = imfilter(sigmas, fspecial('average', window_size), 'replicate');
+ENLs = (means./sigmas).^2;
+sx2s = ((ENLs.*(sigmas).^2) - means.^2)./(ENLs + 1);
+fbar = means + (sx2s.*(I-means)./(sx2s + (means.^2 ./ENLs)));
+OIm(means~=0) = fbar(means~=0);
